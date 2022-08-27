86ee35de749831d252e44161dbd6679d05421519
Nyanthie Mayang <nyanthie.mayang@student.moringaschool.com>
2022-08-28 02:54:30 +0300

    commit tree c3074b755f437bf80d44dd2af27a3f620183cbf2

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song 
+    @@count = 0
+    @@genres = []
+    @@artists = []
+    
+    def self.count
+    @@count
+    end
+    
+    def self.genres
+    @@genres.uniq
+    end
+    
+    def self.artists
+    @@artists.uniq
+    end
+    
+    def self.genre_count
+    @@genres.tally
+    end
+    
+    def self.artist_count
+    @@artists.tally
+    end
+    
+    attr_accessor :name, :artist, :genre
+    
+    def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+    end
+    
+    
+    end 
